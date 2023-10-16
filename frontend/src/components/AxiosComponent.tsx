5841c02c86af1ac546bd66b06fd3eb4b791cad5d
sushant <raosushant045@gmail.com>
2023-10-16 19:59:14 +0530

    fixed bugs

--- frontend/src/components/AxiosComponent.tsx
+++ frontend/src/components/AxiosComponent.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let AxiosInstance = axios.create({
-  baseURL: " http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
